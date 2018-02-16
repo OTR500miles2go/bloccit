b00d1d617b13f7d46eb0030f71fc56cf9b3bd614
OTR500miles2go <32044213+OTR500miles2go@users.noreply.github.com>
2018-02-16 00:01:09 -0600

    Assignment 19 interfaces : complete

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  let(:post) { Question.create!(title: "New Question title", body: "New Question body", resolved: false ) }
+  
+  describe "attributes" do
+    it "has title, body and resolved attributes" do
+      expect(question).to have_attributes(title: "New Question title", body: "New Question body", resolved: false )
+    end
+  end  
+end
